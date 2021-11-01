6a742587d704b2bf4b6192953601de8e9efbbfd6
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-11-01 15:30:28 +0100

    restructure app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import Header from './home/header';
+import ImageToText from './home/imageToText';
+import Footer from './home/footer';
+import ParcelList from './parcel/parcels';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <input type="file" />
-      </header>
+    <div className="">
+      <div className="">
+        <Header />
+      </div>
+      <div className="bg-gray-100">
+        <ImageToText />
+      </div>
+      <div className="bg-white">
+        <ParcelList />
+      </div>
+      <div className="bg-indigo-600">
+        <Footer />
+      </div>
     </div>
   );
 }
