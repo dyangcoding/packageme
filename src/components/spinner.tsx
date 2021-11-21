25b7150979c75f609794c1b55a83bf51e9279d25
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-11-21 02:02:55 +0100

    bugFix: fix spinner

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Spinner = ({ width, high } : { width?: string, high?: string }) => {
     let style = "loader ease-linear rounded-full border-4 border-t-4 border-gray-200 mx-auto";
-    style += width ? width : "w-16 ";
-    style += high ? high : "h-16";
+    style += width ? width : " w-16 ";
+    style += high ? high : " h-16";
     return (
         <div className="text-center py-6">
             <div className={style}></div>
