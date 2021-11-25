f17c4b44fba976eb6f861d94ceb918502c300d2c
DY <Dong.Yang@Student.HTW-Berlin.de>
2021-11-25 12:12:47 +0100

    add feedback form

---
+++ src/components/feedback.tsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { XIcon } from '@heroicons/react/outline';
+
+interface FeedbackProps {
+    readonly onToggleDialog: () => void;
+}
+
+interface FeedbackState {
+    readonly email: string;
+    readonly message: string;
+    readonly submitError?: string;
+}
+
+class Feedback extends React.Component<FeedbackProps, FeedbackState> {
+    constructor(props: FeedbackProps) {
+        super(props);
+
+        this.state = {
+            email: '',
+            message: ''
+        };
+
+        this.onToggleDialog = this.onToggleDialog.bind(this);
+        this.onEmailChange = this.onEmailChange.bind(this);
+        this.onMessageChange = this.onMessageChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    public render(): React.ReactNode {
+        const { email, message } = this.state;
+        return (
+            <div className="fixed bottom-20 right-2 border border-gray-200 shadow w-4/5 md:w-4/5 lg:w-1/4 xl:w-1/6 text-white bg-gray-700 opacity-90 rounded-md">
+                <div className="flex flex-col p-4 mx-auto shadow-sm">
+                    <div className="p-1 bg-white rounded-full ml-auto" onClick={this.onToggleDialog}>
+                        <XIcon className="cursor-pointer text-red-500 h-6 w-6" aria-hidden="true" />
+                    </div>
+                    <p className="font-medium">Feedback</p>
+                    <p className="mt-1 text-sm">If you find a bug or have feedback, don't hesitate to let us know.</p>
+                    <form onSubmit={this.onSubmit}>
+                        <div className="flex flex-col shadow-sm my-2">
+                            <input type="text" name="email" id="email" placeholder="Your Email" value={email} onChange={this.onEmailChange}
+                                className="text-black h-10 p-2 border focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-md sm:text-sm border-gray-300"/>
+                        </div>
+                        <div className="mb-1">
+                            <textarea id="message" name="message" rows={5} placeholder="Your Message" value={message} onChange={this.onMessageChange}
+                                className="text-black p-2 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full text-sm border border-gray-300 rounded-md"/>
+                        </div>
+                        {this.renderErrorMessage()}
+                        <div className="ml-auto">
+                            <button type="submit"
+                                className="my-2 p-1.5 border border-transparent text-sm font-medium rounded-md text-white bg-red-500 hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Send Feedback
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+
+    private renderErrorMessage(): React.ReactNode {
+        const message = this.state.submitError;
+        if (message) {
+            return (
+                <div className="flex items-center text-sm rounded-md mt-4 p-2 bg-red-100 text-red-500">
+                    {message}
+                </div>
+            );
+        }
+        return null;
+    }
+
+    private onEmailChange(event: React.ChangeEvent<HTMLInputElement>): void {
+        this.setState({email: event.target.value, submitError: ''});
+    }
+
+    private onMessageChange(event: React.ChangeEvent<HTMLTextAreaElement>): void {
+        this.setState({message: event.target.value, submitError: ''});
+    }
+
+    private onToggleDialog(): void {
+        this.props.onToggleDialog();
+    }
+
+    private encode(): string {
+        const result: string[] = [];
+        const data = {'form-name': 'feedback', 'email': this.state.email, 'message': this.state.message};
+        for (const [key, value] of Object.entries(data)) {
+            result.push(encodeURIComponent(key) + '=' + encodeURIComponent(value))
+        }
+        return result.join('&');
+    }
+
+    private onSubmit(event: React.FormEvent<HTMLFormElement>): void {
+        event.preventDefault();
+        if (!this.state.email) {
+            this.setState({submitError: 'Email can not be empty.'})
+            return;
+        }
+        if (!this.state.message) {
+            this.setState({submitError: 'Message can not be empty.'});
+            return;
+        }
+        fetch('/', {
+            method: 'POST',
+            headers: {'content-type': 'application/x-www-form-urlencoded'},
+            body: this.encode()
+        })
+        .then(() => this.props.onToggleDialog())
+        .catch(error => this.setState({submitError: error}))
+    }
+}
+
+export default Feedback;
--- src/home/footer.tsx
+++ src/home/footer.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Feedback from '../components/feedback';
 
-class Footer extends React.PureComponent {
-    render() {
+interface FooterProps {}
+
+interface FooterState {
+    readonly toggleDialog: boolean;
+}
+
+class Footer extends React.Component<FooterProps, FooterState> {
+    constructor(props: FooterProps) {
+        super(props);
+        this.state = {
+            toggleDialog: false,
+        };
+
+        this.onToggleDialog = this.onToggleDialog.bind(this);
+    }
+
+    public render(): React.ReactNode {
         return (
             <div className="container mx-auto max-w-6xl flex justify-center items-center h-28 border-t p-5">
                 <div className="container flex flex-col md:flex-row items-center justify-between m-4">
@@ -11,17 +27,22 @@ class Footer extends React.PureComponent {
                     <div className="flex text-sm text-indigo-400 py-2 md:py-0">
                         <a href="/legal" className="px-2 hover:underline">Terms Of Use</a>
                         <button className="px-2 hover:underline" onClick={this.onContactClick}>Contact</button>
-                        <a href="/feedback" className="pl-2 hover:underline">Feedback</a>
+                        <button className="px-2 hover:underline" onClick={this.onToggleDialog}>Feedback</button>
                     </div>
                 </div>
+                {this.state.toggleDialog ? <Feedback onToggleDialog={this.onToggleDialog} /> : null}
             </div>
         );
     }
 
     private onContactClick(_event: React.MouseEvent<HTMLButtonElement>): void {
-        const email = 'hello.packageme@gmail.com';
+        const email = process.env.REACT_APP_CONTACT_EMAIL;
         const subject = 'Questions regarding packageme';
         document.location = 'mailto:' + email + '?subject=' + subject;
+    }
+
+    private onToggleDialog(): void {
+        this.setState(prevState => ({toggleDialog: !prevState.toggleDialog}));
     }
 }
 
